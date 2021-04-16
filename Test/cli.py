a3d292ce3b87f3ff26f4d7b4d5c0130ee7931e64
Sebastian Fricke <sebastian.fricke@posteo.net>
2021-04-16 15:22:48 +0200

    Refactor: cli.py: Convert global variables to class attributes
    Improve the readability and the scope of access to the variables used
    for keeping the order numbers and picture names. Move the variables as
    class attributes into the Windows class.
    
    Signed-off-by: Sebastian Fricke <sebastian.fricke@posteo.net>

--- Test/cli.py
+++ Test/cli.py
@@ -1,7 +1,7 @@
 """
 {BRIEF PROJECT DESCRIPTION}
 
-Copyright (C) 2021  {NAME}
+Copyright (C) 2021  Jan Sallermann, Panasiam
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published
@@ -24,8 +24,8 @@ from loguru import logger
 import tkinter as tk
 from tkinter import ttk
 import smtplib, ssl
-from email.mime.multipart import MIMEMultipart 
-from email.mime.text import MIMEText 
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 import cv2
 from PIL import ImageTk, Image
@@ -63,10 +63,6 @@ email_config = {}
 path = assign_config(config=config, section="PATH", val=path_config)
 email = assign_config(config=config, section="EMAIL", val=email_config)
 
-NUMBERS = []
-NAMES = []
-NAME = ''        
-
 PIC_PATH = path['pic_path']
 PASSWORD = email['password']
 MAIL = email['mail']
@@ -79,20 +75,23 @@ class Windows(tk.Tk):
         tk.Tk.__init__(self)
         self.wm_title("Webcam Panasiam")
         self.geometry('1000x770')
+        self.order_numbers = []
+        self.picture_names = []
+        self.current_picture = ''
 
         container = tk.Frame(self, height=400, width=600)
         container.pack(side="top", fill="both", expand=True)
- 
+
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
- 
+
         self.frames = {}
         for F in (MainPage, SidePage, CompletionScreen):
             frame = F(container, self)
 
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
- 
+
         self.show_frame(MainPage)
 
     def show_frame(self, cont):
@@ -100,32 +99,32 @@ class Windows(tk.Tk):
         frame.tkraise()
 
     def take_picture(self, edit, lb_size, img, lb):
-        NAME=f'Auftrag_{edit}.jpg'
-        NUMBERS.append(edit)
-        NAMES.append(NAME)
+        self.current_picture=f'Auftrag_{edit}.jpg'
+        self.order_numbers.append(edit)
+        self.picture_names.append(self.current_picture)
         cap = cv2.VideoCapture(0)
         while True:
             ret, frame = cap.read()
             if ret == False:
                 break
-            if not len(set(NAMES)) == len(NAMES):
-                NAMES.pop()
-                NUMBERS.pop()
+            if not len(set(self.picture_names)) == len(self.picture_names):
+                self.picture_names.pop()
+                self.order_numbers.pop()
                 messagebox.showerror(
                     'Picture already exists',
-                    str(f"{NAME} already exists please delete {NAME}"))
+                    str(f"{self.current_picture} already exists please delete {self.current_picture}"))
                 return
-            cv2.imwrite(PIC_PATH + NAME, frame)
+            cv2.imwrite(PIC_PATH + self.current_picture, frame)
             break
 
-        load = Image.open(PIC_PATH + NAME)
+        load = Image.open(PIC_PATH + self.current_picture)
         render = ImageTk.PhotoImage(load)
         img.config(image = render)
         img.image = render
-    
+
         cap.release()
         cv2.destroyAllWindows()
-        lb.insert(lb_size,NAME)
+        lb.insert(lb_size, self.current_picture)
 
     def send_email(self, lb_size, lb):
         today = date.today()
@@ -135,18 +134,18 @@ class Windows(tk.Tk):
         message['To'] = RECEIVER
         message['Subject'] = f'Retouren {today}'
 
-        msg_content = 'Anbei befinden sich alle heutigen Retouren. Auftragsnummer steht in dem jeweiligem Titel der angehängten Datei. Die betreffenden Auftragsnummern sind folgende:' + str(NUMBERS)
+        msg_content = 'Anbei befinden sich alle heutigen Retouren. Auftragsnummer steht in dem jeweiligem Titel der angehängten Datei. Die betreffenden Auftragsnummern sind folgende:' + str(self.order_numbers)
         body = MIMEText(msg_content, 'html')
         message.attach(body)
 
-        for name in NAMES:
+        for name in self.picture_names:
             attachment_path = PIC_PATH + name
 
             try:
                 with open(attachment_path, "rb") as attachment:
-                    p = MIMEApplication(attachment.read(),_subtype="jpg")	
+                    p = MIMEApplication(attachment.read(),_subtype="jpg")
                     p.add_header('Content-Disposition',
-                                "attachment; filename= %s" 
+                                "attachment; filename= %s"
                                 % attachment_path.split("/")[-1])
                     message.attach(p)
             except Exception as e:
@@ -157,31 +156,31 @@ class Windows(tk.Tk):
         context = ssl.create_default_context()
 
         with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
-            server.ehlo()  
+            server.ehlo()
             server.starttls(context=context)
             server.ehlo()
             server.login(MAIL, PASSWORD)
-            server.sendmail(MAIL, 
+            server.sendmail(MAIL,
                         message['To'].split(";"),
                         msg_full)
             server.quit()
 
-        for name in NAMES:
+        for name in self.picture_names:
             os.remove(PIC_PATH + name)
             lb.delete(0)
-        
+
         for i in range(lb_size):
-            NAMES.pop()
-            NUMBERS.pop()
+            self.picture_names.pop()
+            self.order_numbers.pop()
 
     def reset(self, lb):
         selection = lb.curselection()
-        os.remove(PIC_PATH + NAMES[selection[0]])
-        NAMES.pop(selection[0])
-        NUMBERS.pop(selection[0])
+        os.remove(PIC_PATH + self.picture_names[selection[0]])
+        self.picture_names.pop(selection[0])
+        self.order_numbers.pop(selection[0])
         lb.delete(selection)
 
-    
+
 
 
 class MainPage(tk.Frame):
@@ -203,22 +202,22 @@ class MainPage(tk.Frame):
             command=lambda: controller.show_frame(CompletionScreen),
         )
         switch_window_button2.grid(row = 1, column = 1)
- 
- 
- 
+
+
+
 class SidePage(tk.Frame):
     def __init__(self, parent, controller):
-        
+
         def img_selection(self):
-            selection = listbox1.curselection()  
+            selection = listbox1.curselection()
             if not selection:
                 return
-            filen = PIC_PATH + NAMES[selection[0]]
+            filen = PIC_PATH + controller.picture_names[selection[0]]
             load = Image.open(filen)
             render = ImageTk.PhotoImage(load)
             img.config(image = render)
             img.image = render
-        
+
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Retouren Auflistung")
         label.grid(row = 0, column = 0, columnspan = 4, sticky = "wens")
@@ -254,7 +253,7 @@ class SidePage(tk.Frame):
         reset_btn = tk.Button(self, text = "Lösche das ausgewählte Bild",
                               command = lambda : controller.reset(lb = listbox1))
         reset_btn.grid(row = 2, column = 2, sticky = "wens")
- 
+
         switch_window_button = tk.Button(
             self,
             text="Zurück zum Menü",
@@ -262,14 +261,14 @@ class SidePage(tk.Frame):
         )
         switch_window_button.grid(
             row = 6, column = 0, columnspan = 4, sticky = "wens")
- 
+
 class CompletionScreen(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Completion Screen, we did it!")
         label.pack(padx=10, pady=10)
         switch_window_button = ttk.Button(
-            self, text="Zurück zum Menü", 
+            self, text="Zurück zum Menü",
             command=lambda: controller.show_frame(MainPage)
         )
         switch_window_button.pack(side="bottom", fill=tk.X)
