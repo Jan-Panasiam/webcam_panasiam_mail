daf4224906ab98f5b4a8bcaa8e6fbbe0edc17d06
Sebastian Fricke <sebastian.fricke@posteo.net>
2021-04-19 16:44:52 +0200

    cli.py: Fix coding style issues
    Remove a duplicate tkinter import, fix import ordering and multiple
    other small coding style issues.
    
    Signed-off-by: Sebastian Fricke <sebastian.fricke@posteo.net>

--- Test/cli.py
+++ Test/cli.py
@@ -21,18 +21,19 @@ import os
 import sys
 import configparser
 import getpass
-import keyring
-from loguru import logger
 import tkinter as tk
 from tkinter import ttk
-import smtplib, ssl
+import smtplib
+import ssl
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
-import cv2
-from PIL import ImageTk, Image
 from datetime import date
 from tkinter import messagebox
+from PIL import ImageTk, Image
+import cv2
+import keyring
+from loguru import logger
 
 
 PROG_NAME = 'panasiam_webcam_mail'
@@ -157,15 +158,15 @@ class Windows(tk.Tk):
             lb          [tk.Listbox]    -   The listbox where the picturename
                                             is to be inserted
         """
-        self.current_picture=f'Auftrag_{edit}.jpg'
+        self.current_picture = f'Auftrag_{edit}.jpg'
         self.order_numbers.append(edit)
         self.picture_names.append(self.current_picture)
         cap = cv2.VideoCapture(0)
         while True:
             ret, frame = cap.read()
-            if ret == False:
+            if ret is False:
                 break
-            if not len(set(self.picture_names)) == len(self.picture_names):
+            if len(set(self.picture_names)) != len(self.picture_names):
                 self.picture_names.pop()
                 self.order_numbers.pop()
                 messagebox.showerror(
@@ -179,7 +180,7 @@ class Windows(tk.Tk):
         load = Image.open(self.config['PATH']['pic_path'] +
                           self.current_picture)
         render = ImageTk.PhotoImage(load)
-        img.config(image = render)
+        img.config(image=render)
         img.image = render
 
         cap.release()
@@ -220,10 +221,10 @@ class Windows(tk.Tk):
 
             try:
                 with open(attachment_path, "rb") as attachment:
-                    p = MIMEApplication(attachment.read(),_subtype="jpg")
+                    p = MIMEApplication(attachment.read(), _subtype="jpg")
                     p.add_header('Content-Disposition',
-                                "attachment; filename= %s"
-                                % attachment_path.split("/")[-1])
+                                 "attachment; filename= %s"
+                                 % attachment_path.split("/")[-1])
                     message.attach(p)
             except Exception as e:
                 print(str(e))
@@ -245,8 +246,8 @@ class Windows(tk.Tk):
                 password = self.get_email_password(email=email, reset=True)
                 server.login(email, password)
             server.sendmail(self.config['EMAIL']['mail'],
-                        message['To'].split(";"),
-                        msg_full)
+                            message['To'].split(";"),
+                            msg_full)
             server.quit()
 
         for name in self.picture_names:
@@ -273,36 +274,33 @@ class Windows(tk.Tk):
         lb.delete(selection)
 
 
-
-
 class MainPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Main Page")
-        label.grid(row = 0, column = 0, columnspan = 2)
+        label.grid(row=0, column=0, columnspan=2)
 
         switch_window_button1 = tk.Button(
             self,
             text="Seite 1",
             command=lambda: controller.show_frame(SidePage),
         )
-        switch_window_button1.grid(row = 1, column = 0)
+        switch_window_button1.grid(row=1, column=0)
 
         switch_window_button2 = tk.Button(
             self,
             text="Seite 2",
             command=lambda: controller.show_frame(CompletionScreen),
         )
-        switch_window_button2.grid(row = 1, column = 1)
-
+        switch_window_button2.grid(row=1, column=1)
 
 
 class SidePage(tk.Frame):
     def __init__(self, parent, controller):
-        def img_selection(self):
+        def img_selection():
             """
-            This function allows the user to get a preview of the selected
-            picture in the listbox.
+            This nested function allows the user to get a preview of the
+            selected picture in the listbox.
             """
             selection = listbox1.curselection()
             if not selection:
@@ -311,60 +309,61 @@ class SidePage(tk.Frame):
                 controller.picture_names[selection[0]]
             load = Image.open(filen)
             render = ImageTk.PhotoImage(load)
-            img.config(image = render)
+            img.config(image=render)
             img.image = render
 
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Retouren Auflistung")
-        label.grid(row = 0, column = 0, columnspan = 4, sticky = "wens")
+        label.grid(row=0, column=0, columnspan=4, sticky="wens")
 
-        label1 = tk.Label(self, text = 'Bitte Auftragsnummer eintragen -->')
-        label1.grid(row = 1, column = 0, sticky="wens")
+        label1 = tk.Label(self, text='Bitte Auftragsnummer eintragen -->')
+        label1.grid(row=1, column=0, sticky="wens")
 
-        label2 = tk.Label(self, text = 'Bilder die versendet werden:')
-        label2.grid(row = 4, column = 0, sticky="we")
+        label2 = tk.Label(self, text='Bilder die versendet werden:')
+        label2.grid(row=4, column=0, sticky="we")
 
         img = tk.Label(self)
-        img.grid( row = 5, column = 1, columnspan = 3, sticky = "wens")
+        img.grid(row=5, column=1, columnspan=3, sticky="wens")
 
         edit1 = tk.Entry(self)
-        edit1.grid(row = 1, column = 1, columnspan = 3, sticky = "wens")
+        edit1.grid(row=1, column=1, columnspan=3, sticky="wens")
 
-        listbox1 = tk.Listbox(self, width = 35)
+        listbox1 = tk.Listbox(self, width=35)
         listbox1.bind('<<ListboxSelect>>', img_selection)
-        listbox1.grid(row = 4, column = 1, columnspan = 3, sticky = "wens")
+        listbox1.grid(row=4, column=1, columnspan=3, sticky="wens")
 
-        picture_btn = tk.Button(self, text = "Schieße ein Foto",
-                                command = lambda : controller.take_picture(
-                                edit = edit1.get(),
-                                lb_size = listbox1.size(),
-                                img = img, lb = listbox1))
-        picture_btn.grid(row = 2, column = 1, sticky = "wens")
+        picture_btn = tk.Button(self, text="Schieße ein Foto",
+                                command=lambda: controller.take_picture(
+                                    edit=edit1.get(),
+                                    lb_size=listbox1.size(),
+                                    img=img, lb=listbox1))
+        picture_btn.grid(row=2, column=1, sticky="wens")
 
-        send_btn = tk.Button(self, text = "Versende die Bilder per e-Mail",
-                             command = lambda : controller.send_email(
-                             lb_size = listbox1.size(), lb = listbox1))
-        send_btn.grid(row = 2, column = 3, sticky = "wens")
+        send_btn = tk.Button(self, text="Versende die Bilder per e-Mail",
+                             command=lambda: controller.send_email(
+                                 lb_size=listbox1.size(), lb=listbox1))
+        send_btn.grid(row=2, column=3, sticky="wens")
 
         reset_btn = tk.Button(
-            self, text = "Lösche das ausgewählte Bild",
-            command = lambda : controller.reset(lb = listbox1))
-        reset_btn.grid(row = 2, column = 2, sticky = "wens")
+            self, text="Lösche das ausgewählte Bild",
+            command=lambda: controller.reset(lb=listbox1))
+        reset_btn.grid(row=2, column=2, sticky="wens")
 
-        switch_window_button = tk.Button(
+        switch_window_button = ttk.Button(
             self,
             text="Zurück zum Menü",
             command=lambda: controller.show_frame(MainPage),
         )
         switch_window_button.grid(
-            row = 6, column = 0, columnspan = 4, sticky = "wens")
+            row=6, column=0, columnspan=4, sticky="wens")
+
 
 class CompletionScreen(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Seite 2")
         label.pack(padx=10, pady=10)
-        switch_window_button = ttk.Button(
+        switch_window_button = tk.Button(
             self, text="Zurück zum Menü",
             command=lambda: controller.show_frame(MainPage)
         )
@@ -378,5 +377,5 @@ def main():
         logger.error("Die Konfiguration muss angepasst werden.")
         sys.exit(1)
 
-    testObj = Windows(config=config)
-    testObj.mainloop()
+    gui = Windows(config=config)
+    gui.mainloop()
